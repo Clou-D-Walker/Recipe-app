09d1a581f19eb0e0ccf76abd2298c1f153bbeec7
Surya Sujit <35captainmarvel@gmail.com>
2025-08-26 18:09:30 +0530

    configured db

--- server/index.js
+++ server/index.js
@@ -6,9 +6,18 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
 const router = express.Router();
+const connectDB = require("./db/config");
+
+connectDB(process.env.MONGODB_URI);
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const config = require("./db/config");
 const Home = require("./controllers/controller");
